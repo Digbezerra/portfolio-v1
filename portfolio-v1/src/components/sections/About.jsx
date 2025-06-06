879a9e0064dbfa0598609603c030189083c1f1fd
Diego Bezerra <diegobmartins21@gmail.com>
2025-06-06 14:55:53 -0300

    Section Jobs - Structure and design

--- portfolio-v1/src/components/sections/About.jsx
+++ portfolio-v1/src/components/sections/About.jsx
@@ -11,24 +11,24 @@ export function About() {
           </div>
           <div className="section-about__content">
             <div>
-              <p>
+              <p className="paragraph">
                 Olá, sou o Diego (Dig) e construo ferramentas focadas em
                 acessibilidade, experiência do usuário, escalabilidade e
                 performance.
               </p>
-              <p>
+              <p className="paragraph">
                 Atuei como Frontend developer durante cinco anos incríveis, onde
                 tive a oportunidade de trabalhar com diversas empresas
                 multinacionais, como: Ambev, Hortifrutti, Heckitt, Adobe e
                 Nestlé.
               </p>
-              <p>
+              <p className="paragraph">
                 Exerci durante 1 ano e meio a função de Engenheiro de Software
                 Jr, atuei como tech lead de frontend no projeto Nestlé Até Você,
                 um grande projeto B2B onde eu era responsável por arquitetar e
                 gerenciar as implementações do time de frontend.
               </p>
-              <p>
+              <p className="paragraph">
                 Fui mentor de diversos devs incríveis e aprendi muito com isso,
                 hoje sinto que sou um profissional mais completo.
               </p>
